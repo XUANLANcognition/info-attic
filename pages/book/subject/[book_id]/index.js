0fff8e485a634135eadbb068a2d39e91d0ea38a1
xuanlan <1352512358@qq.com>
2022-04-08 18:37:49 +0800

    修复没有介绍内容时，宽度塌缩的问题

--- pages/book/subject/[book_id]/index.js
+++ pages/book/subject/[book_id]/index.js
@@ -69,6 +69,7 @@ function BookSubject(props) {
       <main className={styles.main}>
         <div
           style={{
+            width: "100%",
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
